3578412d9e6346214b739d5a108a5118f6a59662
Node Central <chris_parker_uk@hotmail.co.uk>
2022-01-08 17:04:23 +0000

    Add files via upload

--- L_ExternalIP1.lua
+++ L_ExternalIP1.lua
@@ -1,6 +1,6 @@
 module("L_ExternalIP1", package.seeall)
 
-local PV = "0.2" -- plugin version number
+local PV = "0.3" -- plugin version number
 local COM_SID = "urn:nodecentral-net:serviceId:ExternalIP1"
 
 function log(msg) 
@@ -8,35 +8,45 @@ function log(msg)
 end
 
 function refreshExternalIP(lul_device)
+	log("Refreshing Address for #" ..lul_device)
+	
+	local deviceID = tonumber(lul_device)
 	local time = os.time()
+	local HRtime = os.date( "%d/%m @ %H:%M" , time )
+	local PollPeriod = luup.variable_get(COM_SID, "Poll Period", deviceID)
+	log("PollPeriod = " .. PollPeriod)
 	
 	local status, ip = luup.inet.wget("https://api.ipify.org/?format=txt")
 	local liveIP = ip: match "%d+%.%d+%.%d+%.%d+"
-	log("External IP address check returned = " ..liveIP)
+	log("Address check returned = " ..liveIP)
 	
-	local existingIP, lastUpdateTime1 = luup.variable_get(COM_SID,"Current External IP", lul_device)
-	local previousIP, lastUpdateTime2 = luup.variable_get(COM_SID,"Previous External IP", lul_device)
+	local existingIP = luup.variable_get(COM_SID,"Current External IP", deviceID)
+	log("Existing IP = " .. existingIP)
 	
-	luup.variable_set(COM_SID, "Last Checked", time , lul_device)
-	luup.variable_set(COM_SID, "Last Checked HR", time , lul_device)
+	local previousIP = luup.variable_get(COM_SID,"Previous External IP", deviceID)
+	log("previousIP = " .. previousIP)
+	
+	luup.variable_set(COM_SID, "Last Checked", time , deviceID)
+	luup.variable_set(COM_SID, "Last Checked HR", HRtime , deviceID)
 	
 	if liveIP ~= existingIP then
-		log("External IP has changed from " ..existingIP .. " to " ..liveIP)
-		luup.variable_set(COM_SID, "Previous External IP", existingIP, lul_device)
-		luup.variable_set(COM_SID, "Current External IP", liveIP, lul_device)
-		luup.variable_set(COM_SID, "Last Changed", time , lul_device)
-		luup.variable_set(COM_SID, "Last Changed HR", time , lul_device)
+		log("Address has changed from " .. existingIP .. " to " ..liveIP)
+		luup.variable_set(COM_SID, "Previous External IP", existingIP, deviceID)
+		luup.variable_set(COM_SID, "Current External IP", liveIP, deviceID)
+		luup.variable_set(COM_SID, "Last Changed", time , deviceID)
+		luup.variable_set(COM_SID, "Last Changed HR", HRtime , deviceID)
+		log("External IP Address has been updated")
 	else 
 		log("External IP Address has NOT changed")
 	end
 	
-	luup.call_delay ("refreshExternalIP", 86400) -- Check if the IP address has changed ever 24hours
-
+	luup.call_delay ("NC_RefreshMyExtIP", PollPeriod, deviceID) -- period to check if IP address has changed
 end
 
+
 local function populateFixedVariables(lul_device)
-	log("Setting up plugins variables for ... DevNo = " ..lul_device)
-	luup.variable_set(COM_SID, "PluginStatus", "Plugin variables being set up 2/2", lul_device)
+	log("Setting up fixed variables for ... DevNo = " ..lul_device)
+	luup.variable_set(COM_SID, "PluginStatus", "Plugin variables being set up 2/3", lul_device)
 	
 	local CurrExternalIP = luup.variable_get(COM_SID, "Current External IP", lul_device)
 		if (CurrExternalIP == nil) then luup.variable_set(COM_SID, "Current External IP", "0.0.0.0" , lul_device) end
@@ -55,7 +65,14 @@ local function populateFixedVariables(lul_device)
 	
 	local LastCheckedHR = luup.variable_get(COM_SID, "Last Checked HR", lul_device)
 		if (LastCheckedHR == nil) then luup.variable_set(COM_SID, "Last Checked HR", "Date" , lul_device) end
-	refreshExternalIP(lul_device)
+		
+	local PollPeriod = luup.variable_get(COM_SID, "Poll Period", lul_device)
+		if (PollPeriod == nil) then luup.variable_set(COM_SID, "Poll Period", 86400 , lul_device) end
+	
+	luup.variable_set(COM_SID, "PluginStatus", "Plugin variables are set up 3/3"
+	
+		refreshExternalIP(lul_device)
+		
 end
 
 function ExternalIPStartUp(lul_device)
@@ -63,6 +80,6 @@ function ExternalIPStartUp(lul_device)
 	luup.variable_set(COM_SID, "Icon", 0, lul_device)
 	luup.variable_set(COM_SID, "PluginVersion", PV, lul_device)
 	luup.variable_set(COM_SID, "Debug", true, lul_device)
-	luup.variable_set(COM_SID, "PluginStatus", "Plugin being installed 1/2 ", lul_device)
+	luup.variable_set(COM_SID, "PluginStatus", "Plugin being installed 1/3 ", lul_device)
 	populateFixedVariables(lul_device)
 end
